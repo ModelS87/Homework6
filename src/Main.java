b54642306b25ee3b4d739572fc6aea8d2892b49a
volynkina <bzzz18@yandex.ru>
2023-01-10 08:34:15 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         for (int a = 1; a <= 10; a = a + 1) {
             System.out.println(a);
         }
-
+for (int b = 10; b >=1; b = b - 1) {
+    System.out.println(b);
+}
     }
 }
