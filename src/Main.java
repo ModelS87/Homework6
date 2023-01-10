6a7da73146bc4ae6ed1a2cd98c660a7478d83076
volynkina <bzzz18@yandex.ru>
2023-01-10 08:30:07 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        for (int a = 1; a <= 10; a = a + 1) {
+            System.out.println(a);
+        }
+
     }
 }
