ee1d0d8a7ab0d0cf4ad5d55584b7938ed5671491
volynkina <bzzz18@yandex.ru>
2023-01-10 08:52:16 +0500

    task7

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,8 @@ for (int e = 1904; e<2096; e = e+4) {
 for (int f = 7; f<=98; f = f+7) {
     System.out.println(f);
 }
+for (int g = 1; g<=512; g = g*2)   {
+    System.out.println(g);
+}
     }
 }
